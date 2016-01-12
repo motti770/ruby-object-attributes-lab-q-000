5ead935d6eb657a46c16d39b226cfc321638c690
motti770 <mottigottlieb@gmail.com>
2016-01-12 19:21:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+ class Dog
+   def name
+     @name
+   end
+
+   def name=(name_value)
+     @name = name_value
+   end
+   
+   def breed
+     @breed
+   end
+   
+   def breed=(breed_value)
+     @breed = breed_value
+   end
+ end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(name_value)
+    @name = name_value
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job_value)
+    @job = job_value
+  end
+end
